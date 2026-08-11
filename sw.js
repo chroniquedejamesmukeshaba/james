e23c30755c96fdc99c3bc3573d2a4da1d82ec1e6
YagirwaGedeon <gedeonmukeshaba@gmail.com>
2026-08-11 14:03:30 +0200

    fix: service worker cache v8 (assets a jour, menu mobile)

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE = 'chronique-v7';
+const CACHE = 'chronique-v8';
 const STATIC = [
   '/', '/index.html', '/actualites.html', '/recherche', '/categorie/societe',
   '/qui-sommes-nous.html', '/projets.html', '/sensibilisation.html',
-  '/objets-perdus.html', '/heritage.html', '/faq.html', '/donation.html',
-  '/transparence.html', '/page.html', '/article.html',
-  '/css/style.css', '/css/home.css', '/js/main.js', '/js/admin.js', '/js/data.js', '/js/i18n.js', '/js/search.js', '/js/notify.js', '/js/home.js', '/js/donation.js', '/js/actions.js',
+  '/objets-perdus.html', '/heritage.html', '/faq.html', '/soutenir.html',
+  '/transparence.html', '/donation.html', '/page.html', '/article.html',
+  '/categorie.html', '/recherche.html', '/cookies.html', '/privacy.html',
+  '/mentions-legales.html',
+  '/css/tokens.css', '/css/style.css', '/css/home.css', '/css/redesign.css',
+  '/js/main.js', '/js/admin.js', '/js/data.js', '/js/i18n.js', '/js/search.js', '/js/notify.js', '/js/home.js', '/js/donation.js', '/js/actions.js', '/js/consent.js',
   '/manifest.json', '/assets/images/logo.png',
   '/assets/images/icon-192.png', '/assets/images/icon-512.png'
 ];
