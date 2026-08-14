10cc5629bb5413f30e83f09d196e7becfb0aaf33
YagirwaGedeon <gedeonmukeshaba@gmail.com>
2026-08-14 04:56:17 +0200

    Pages secondaires 2026: heros, cards, formulaires, publicite, transparence + SW v23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chronique-v22';
+const CACHE = 'chronique-v23';
 const STATIC = [
   '/', '/index.html', '/actualites.html', '/recherche', '/categorie/societe',
   '/qui-sommes-nous.html', '/projets.html', '/sensibilisation.html',
