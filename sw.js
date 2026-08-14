b651adc5c7d7ccab631602e6c7c41ff50f94f12e
YagirwaGedeon <gedeonmukeshaba@gmail.com>
2026-08-14 02:51:38 +0200

    SW v19 (rafraichit les caches apres corrections admin)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chronique-v18';
+const CACHE = 'chronique-v19';
 const STATIC = [
   '/', '/index.html', '/actualites.html', '/recherche', '/categorie/societe',
   '/qui-sommes-nous.html', '/projets.html', '/sensibilisation.html',
